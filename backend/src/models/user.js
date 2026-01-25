9adaecee388b0a63fb48bb63073dfd15031d24fe
bhargav <141304156+bala-bhargav@users.noreply.github.com>
2026-01-25 08:40:02 +0530

    profile created

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,21 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, unique: true, sparse: true },
-    phone: { type: String, unique: true, sparse: true },
-    password: { type: String },
-    role: {
-      type: String,
-      enum: ["farmer", "user", "consumer"],
-      default: "user",
+    {
+        name: { type: String, required: true },
+        email: { type: String, unique: true, sparse: true },
+        phone: { type: String, unique: true, sparse: true },
+        password: { type: String },
+        role: {
+            type: String,
+            enum: ["farmer", "user", "consumer"],
+            default: "user",
+        },
+        googleId: { type: String, unique: true, sparse: true },
+        otp: { type: String },
+        otpExpiry: { type: Date },
     },
-    googleId: { type: String, unique: true, sparse: true },
-    otp: { type: String },
-    otpExpiry: { type: Date },
-  },
-  { timestamps: true },
+    { timestamps: true },
 );
 
 module.exports = mongoose.model("User", userSchema);
